3ff0d83b553371f3e7468f0f51e414aa627b6657
Paul Moscoso <amop@APSOFT.SA>
2026-04-30 15:03:08 +0200

    refactor(client): remove Client feature

--- eTeller.Application/Features/Client/Queries/GetClient/GetClientQuery.cs
+++
@@ -1,9 +0,0 @@
-using eTeller.Application.Mappings.Client;
-using MediatR;
-
-namespace eTeller.Application.Features.Client.Queries.GetClient
-{
-    public class GetClientQuery : IRequest<IEnumerable<ClientVm>>
-    {
-    }
-}
